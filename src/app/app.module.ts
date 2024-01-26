e6abc7e022d2003590de3af3971ba3fcb47833b7
Daniel López del Valle <daniel.lopez@treelogic.com>
2024-01-26 14:40:11 +0100

    Fin de sección 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroModule } from './heroes/components/heroes.module';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +16,7 @@ import { HeroModule } from './heroes/components/heroes.module';
     AppRoutingModule,
     CounterModule,
     HeroModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
